c1f469b356ae126e4d28f678e2fccf46496451b5
Vincenzo Manini <79824675+anghelomanini12@users.noreply.github.com>
2025-01-04 21:18:46 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function seleccionar(link) {
+    var opciones = document.querySelectorAll('#links  a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+    opciones[5].className = "";    
+    link.className = "seleccionado";
+
+  
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
+function responsiveMenu() {
+    var x = document.getElementById("nav");
+    if (x.className === "") {
+        x.className = "responsive";
+    } else {
+        x.className = "";
+    }
+}
+
+
